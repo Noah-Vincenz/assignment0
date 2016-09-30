d5f3d9e4cf4cf819d7f3ef2ef27eea62e2c1b74f
Noah-Vincenz <Noah@noahvincenzsmbp.home>
2016-09-30 19:35:45 +0100

    Tried the screenAsBytes.
    When I print it out it at first it seems like it is done the right way,
    because when printing out every element in my screen array it will print
    00100000 as the first byte, which should be there and should be 32 in
    decimal. However, when I print out every element in the binary array
    'screen' java will print decimals and instead of printing 32 it somehow
    prints -96, but 32 for the corresponding byte in the 'answer' array.

--- ScreenAsBytes.java
+++ ScreenAsBytes.java
@@ -1,7 +1,34 @@
 public class ScreenAsBytes {
 
     public static void setPixel(byte[] screen, int width, int x, int y) {
-        // TODO complete this code
+        int arraySize = screen.length;
+        int byteWidth = width / 8;
+        int height = screen.length / byteWidth; 
+        int count = 0;
+        String stringOfInts = "";
+        for (int i1 = 0; i1 < height; i1++) {
+        	for (int i2 = 0; i2 < width; i2++) {
+        		if (i2 == x && i1 == y) {
+        			stringOfInts += Integer.toString(1);
+        		}
+        		else {
+        			stringOfInts += Integer.toString(0);
+        		}
+        		count++;
+            }
+        }
+        String[] stringOfBytes = stringOfInts.split("(?<=\\G.{8})");
+        int count2 = 0;
+        for (String s : stringOfBytes) {
+        	System.out.println (s); //just to check if its right up to here
+            byte b = (byte) Integer.parseInt(s);
+            screen[count2] = b;
+            count2++;
+            
+        }
+        for (byte b : screen) {
+        	System.out.println(b);
+        }
     }
 
     public static void drawHorizontalLine(byte[] screen, int width, int startX, int endX, int y) {
