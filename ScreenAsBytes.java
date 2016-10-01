4e9b0bf4b42d4d94f9e077155408b10ec09d8dd7
Noah-Vincenz <Noah@noahvincenzsmbp.home>
2016-10-01 14:00:33 +0100

    All tests passed. Not sure whether it is right to store the bytes as decimals in the array, but will inform myself about that. Now going to clean the code up and encapsulate some bits

--- ScreenAsBytes.java
+++ ScreenAsBytes.java
@@ -1,7 +1,6 @@
 public class ScreenAsBytes {
 
     public static void setPixel(byte[] screen, int width, int x, int y) {
-        int arraySize = screen.length;
         int byteWidth = width / 8;
         int height = screen.length / byteWidth; 
         int count = 0;
@@ -20,24 +19,59 @@ public class ScreenAsBytes {
         String[] stringOfBytes = stringOfInts.split("(?<=\\G.{8})");
         int count2 = 0;
         for (String s : stringOfBytes) {
-        	System.out.println (s); //just to check if its right up to here
-            byte b = (byte) Integer.parseInt(s);
-            screen[count2] = b;
+            int i = Integer.parseInt(s, 2); //parses string s as signed integer in radix specified (2) – this returns the decimal of the byte
+            screen[count2] = (byte) i;
             count2++;
-            
-        }
-        for (byte b : screen) {
-        	System.out.println(b);
         }
     }
 
     public static void drawHorizontalLine(byte[] screen, int width, int startX, int endX, int y) {
-        // TODO complete this code
+    	int byteWidth = width / 8;
+        int height = screen.length / byteWidth; 
+        int count = 0;
+        String stringOfInts = "";
+        for (int i1 = 0; i1 < height; i1++) {
+        	for (int i2 = 0; i2 < width; i2++) {
+        		if (i1 == y && i2 >=startX && i2 <= endX){
+        			stringOfInts += Integer.toString(1);
+        		}
+        		else {
+        			stringOfInts += Integer.toString(0);
+        		}	
+        		count++;
+            }
+        }
+        String[] stringOfBytes = stringOfInts.split("(?<=\\G.{8})");
+        int count2 = 0;
+        for (String s : stringOfBytes) {
+            int i = Integer.parseInt(s, 2);
+            screen[count2] = (byte) i;
+            count2++;
+        }
     }
     
     public static void drawVerticalLine(byte[] screen, int width, int x, int startY, int endY) {
-        // TODO complete this code
+    	int byteWidth = width / 8;
+        int height = screen.length / byteWidth; 
+        int count = 0;
+        String stringOfInts = "";
+        for (int i1 = 0; i1 < height; i1++) {
+        	for (int i2 = 0; i2 < width; i2++) {
+        		if (i2 == x && i1 >= startY && i1 <= endY) {
+        			stringOfInts += Integer.toString(1);
+        		}
+        		else {
+        			stringOfInts += Integer.toString(0);
+        		}	
+        		count++;
+            }
+        }
+        String[] stringOfBytes = stringOfInts.split("(?<=\\G.{8})");
+        int count2 = 0;
+        for (String s : stringOfBytes) {
+            int i = Integer.parseInt(s, 2);
+            screen[count2] = (byte) i;
+            count2++;
+        }
     }
-
-
 }
